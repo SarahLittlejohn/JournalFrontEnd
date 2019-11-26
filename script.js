75cd8bacfd0a98a03e6e37fadc735d57b919135d
Sarah <sarahjanelittlejohn@gmail.com>
2019-11-26 10:29:53 +0000

    Create_CRUD_functions: introducing functions

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ resources.open('GET', localhost:5432/journal, false);
 resources.send();
 resources.responseType = 'text';
 
+resources.onload = function() {
+    // Contains functions e.g. remove, create,... that work when data is loaded.
+    var data = JSON.parse(this.response);
+    
+    // Function to display resources
+    display.resources(data);
+
+    // Function to create a resource
+    createResource();
+
+    // FUnction to delete a resource
+    deleteResource();
+}
+
+// Functions are defined here
+// Create resource button
+
+// Delete resource button
