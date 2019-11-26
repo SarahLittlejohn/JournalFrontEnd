6b22cd5b4acb70890550c788e1360219f980353c
Sarah <sarahjanelittlejohn@gmail.com>
2019-11-26 17:14:22 +0000

    Create_CRUD_functions: odd changes

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 // Make http request for resources from database
 let resources = new XMLHttpRequest();
-resources.open('GET', localhost:5432/journal, false);
+resources.open('GET', "http://localhost:5432/journal", false);
 resources.send();
 resources.responseType = 'text';
 
 // Functions e.g. remove, create,... that work when data is loaded
 resources.onload = function() {
-    
+
     var data = JSON.parse(this.response);
-    
+
     // Function to display resources
     displayResources(data);
 
     // Function to create a resource
-    createResource();
+    createResourceButton();
 
     // FUnction to delete a resource
     deleteResourceButton();
+
 }
 
 // Functions are defined here
@@ -26,15 +27,18 @@ function displayResources(data) {
 }
 
 // Create resource button
-function createResource() {
+function createResourceButton() {
+    buttonId.addEventListener('click', createResource());
+  }
 
+// Function for creating resource (via button)
+function createResource(){
+    
 }
 
 // Delete resource button
 function deleteResourceButton(buttonId, elementToRemoveId) {
-    buttonId.addEventListener('click', () => {
-      deleteResource(elementToRemoveId);
-    });
+    buttonId.addEventListener('click', deleteResource(elementToRemoveId));
   }
 
   // Function for removing an element (via button)
