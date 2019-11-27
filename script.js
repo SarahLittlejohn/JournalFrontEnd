4c207bc2771060029dfaa042011290bdf3a9b4d0
Sarah <sarahjanelittlejohn@gmail.com>
2019-11-27 12:37:36 +0000

    Create_CRUD_functions: small changes

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const resourceDisplay = document.querySelectorAll();
 
 // Make http request for resources from database
 let resources = new XMLHttpRequest();
-resources.open('GET', 'http://localhost:8094/resources', false);
-resources.send();
-resources.responseType = 'text';
+resources.open('GET', 'http://localhost:5432', true);
+
+//resources.responseType = 'text';
 
 // Functions e.g. remove, create,... that work when data is loaded
 resources.onload = function() {
 
-    var data = JSON.parse(this.response);
+    var data = JSON.parse(resources.response);
 
     // Function to display resources
-    displayResources(data);
+    //displayResources(data);
 
     // Function to create a resource
     createResourceButton();
@@ -25,19 +25,19 @@ resources.onload = function() {
 
 }
 
+resources.send();
+
 // Functions are defined here
 // Display resources
 function displayResources(data) {
-
-  
 // Display data in the form of a table
-
+    document.getElementById("demo").innerHTML = resources.response;
 }
 
 
 // Create resource button
 function createResourceButton() {
-    document.getElementById("demo").innerHTML = "Hello world";
+    document.getElementById("demo").innerHTML = "Test create button";
 }
 
 // Function for creating resource (via button)
@@ -47,7 +47,8 @@ function createResource(){
 
 // Delete resource button
 function deleteResourceButton(buttonId, elementToRemoveId) {
-    buttonId.addEventListener('click', deleteResource(elementToRemoveId));
+  document.getElementById("demo").innerHTML = "Test delete button";
+    // buttonId.addEventListener('click', deleteResource(elementToRemoveId));
   }
 
   // Function for removing an element (via button)
@@ -58,7 +59,8 @@ function deleteResource(elementId) {
 
   // Update resource button
   function updateResourceButton(buttonId, elementToUpdateId) {
-    buttonId.addEventListener('click', updateResource(elementToUpdateId));
+    document.getElementById("demo").innerHTML = "Test update button";
+    //buttonId.addEventListener('click', updateResource(elementToUpdateId));
   }
 
   // Function for updating an element (via button)
